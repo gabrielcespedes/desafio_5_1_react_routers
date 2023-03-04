893ef4344a207cff193fd1de638d49ad0b6fc616
Gabriel Céspedes <gabrielcespedesalarc@gmail.com>
2023-03-04 19:06:17 -0300

    version estable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default () => {
+    return(
+        <div id="navbar" className="bg-danger text-light">
+            <div id="navbar_1" className="ms-3">
+                <h6>🏠 Home</h6>
+                <h6 className="ms-2">📒 Contacto</h6>
+            </div>
+            <div id="navbar_2" className="me-3">
+                <h5>Happy Cake 🍰</h5>
+            </div>            
+        </div>
+    )
+}
